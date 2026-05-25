a060eb37193dfe2a51a0ad4d2b73f737376169db
jmodeler <srs282@cornell.edu>
2026-05-25 16:26:31 -0400

    added solution for valid anagram

--- prep_days/day_1/valid_anagram.py
+++ prep_days/day_1/valid_anagram.py
@@ -1,18 +1,17 @@
 """
 Valid Anagram — Day 1, Problem 4
 Target time: 5 min
-Actual time: ___
+Actual time: 40 sec
 
 Clarifying questions (write 2–3 BEFORE coding):
-- ?
+- Sanity check for inputs?
 - ?
 - ?
 """
 
 
 def is_anagram(s: str, t: str) -> bool:
-    # your code here
-    ...
+    return sorted(s) == sorted(t)
 
 
 if __name__ == "__main__":
@@ -20,4 +19,5 @@ if __name__ == "__main__":
     assert is_anagram("rat", "car") is False
     assert is_anagram("", "") is True
     assert is_anagram("a", "ab") is False
+    assert is_anagram("aab", "abb") is False
     print("ok")
